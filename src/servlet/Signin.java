576f18e46671a44b915e692864ab21419f1f3159
fracos23 <fracos23@gmail.com>
2017-04-19 11:50:08 +0200

    Servlet signin

---
+++ src/servlet/Signin.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import connection.UserConnection;
+
+/**
+ * Servlet implementation class Signin
+ */
+@WebServlet("/Signin")
+public class Signin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Signin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nickname = request.getParameter("nickname");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		System.out.println(email + password);
+		JSONObject profile = new UserConnection().createProfile(email, nickname, password);
+		if (profile == null) {
+			response.getWriter().append("");
+		} else {
+			response.getWriter().append(profile.toString());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
