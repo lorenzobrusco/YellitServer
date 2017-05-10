54e8ef0c6bdbd5368bc766b2cff16a4caddd51bc
fracos23 <fracos23@gmail.com>
2017-05-10 09:49:42 +0200

    # WARNING: head commit changed in the meantime
    added uploadinfo servlet, add and remove like servlet, modificato login,
    l'userconnection per l'upload e likeconnection

--- src/connection/PostConnection.java
+++ src/connection/PostConnection.java
@@ -16,22 +16,25 @@ public class PostConnection extends AbstractDBManager {
 
 	public JSONArray getAllPost(final String data) {
 		final JSONArray posts = new JSONArray();
-		//final String query = "select * from post where date = ?;";
+		// final String query = "select * from post where date = ?;";
 		final String query = "select * from post;";
 		try {
 			final Connection mConnection = createConnection();
 			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
-			
+
 			// mPreparedStatement.setString(1, data);
-			// new UserConnection().getProfileImage(mResultSet.getString("user"))
-			
+			// new
+			// UserConnection().getProfileImage(mResultSet.getString("user"))
+
 			final ResultSet mResultSet = mPreparedStatement.executeQuery();
 			while (mResultSet.next()) {
 				/*
-				 * Manca da prendere il parametro TEXT, mo intanto vediamo se funziona
+				 * Manca da prendere il parametro TEXT, mo intanto vediamo se
+				 * funziona
 				 */
 				final JSONObject post = new JSONObject();
-				post.put("id_post", mResultSet.getString("idPost"));
+				final int idPost = Integer.parseInt(mResultSet.getString("idPost"));
+				post.put("id_post", idPost + "");
 				post.put("user_nick", mResultSet.getString("user_email"));
 				post.put("user_image", "null");
 				post.put("type", mResultSet.getString("type"));
@@ -39,7 +42,8 @@ public class PostConnection extends AbstractDBManager {
 				post.put("position", mResultSet.getString("position"));
 				post.put("post_image", mResultSet.getString("image"));
 				post.put("post_video", "null");
-				post.put("likes", "null");		
+				final int likes = new LikeConnection().getLikesPost(idPost);
+				post.put("likes", likes+"");
 				posts.put(post);
 			}
 			closeConnection();
@@ -51,13 +55,16 @@ public class PostConnection extends AbstractDBManager {
 		return posts;
 	}
 
-	public boolean createPost(final String type, final String position, final String image, final String userEmail, final String text) {
-		
-		//final String query = "INSERT INTO `yellit`.`post` (`data`, `title`, `subtitle`, `type`, `text`, `location`) VALUES (?, ?, ?, ?, ?, ?, ?);";
-		
+	public boolean createPost(final String type, final String position, final String image, final String userEmail,
+			final String text) {
+
+		// final String query = "INSERT INTO `yellit`.`post` (`data`, `title`,
+		// `subtitle`, `type`, `text`, `location`) VALUES (?, ?, ?, ?, ?, ?,
+		// ?);";
+
 		final String query = "INSERT INTO post (post.type, post.position, post.image, post.user_email, post.text) VALUES (?, ?, ?, ?, ?);";
 		try {
-			
+
 			final Connection mConnection = createConnection();
 			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
 			mPreparedStatement.setString(1, type);
@@ -66,17 +73,17 @@ public class PostConnection extends AbstractDBManager {
 			mPreparedStatement.setString(4, userEmail);
 			mPreparedStatement.setString(5, text);
 			mPreparedStatement.execute();
-			
+
 			closeConnection();
-			
+
 			return true;
-			
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 			return false;
-			
-		} 		
+
+		}
 	}
 
 	public JSONObject deletePost(final int id) {
@@ -95,7 +102,7 @@ public class PostConnection extends AbstractDBManager {
 		}
 		return deletePost;
 	}
-	
+
 	public JSONArray getPersonalPost(final String email) {
 		final JSONArray posts = new JSONArray();
 		final String query = "select count(*), type from post where user_email = ? group by type ;";
@@ -108,8 +115,8 @@ public class PostConnection extends AbstractDBManager {
 			while (mResultSet.next()) {
 				final JSONObject post = new JSONObject();
 				post.put("", mResultSet.getString("count(*)"));
-				post.put("", mResultSet.getString("type"));	
-				
+				post.put("", mResultSet.getString("type"));
+
 				posts.put(post);
 			}
 			closeConnection();
