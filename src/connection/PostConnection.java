b3b7b37032211421a5e318b678a8fcdb9611b0b0
SalvaIsa <salvaisa7@libero.it>
2017-05-12 16:13:46 +0200

    Getting User nick and image

--- src/connection/PostConnection.java
+++ src/connection/PostConnection.java
@@ -15,49 +15,71 @@ public class PostConnection extends AbstractDBManager {
 	}
 
 	public JSONArray getAllPost(final String data) {
-		final JSONArray posts = new JSONArray();
-		//final String query = "select * from post where date = ?;";
+
+		final JSONArray postsList = new JSONArray();
 		final String query = "select * from post;";
+
 		try {
+
 			final Connection mConnection = createConnection();
 			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
-			
-			// mPreparedStatement.setString(1, data);
-			// new UserConnection().getProfileImage(mResultSet.getString("user"))
-			
+
 			final ResultSet mResultSet = mPreparedStatement.executeQuery();
 			while (mResultSet.next()) {
-				/*
-				 * Manca da prendere il parametro TEXT, mo intanto vediamo se funziona
-				 */
+
 				final JSONObject post = new JSONObject();
+
 				post.put("id_post", mResultSet.getString("idPost"));
 				post.put("user_nick", mResultSet.getString("user_email"));
-				post.put("user_image", "null");
 				post.put("type", mResultSet.getString("type"));
 				post.put("date", mResultSet.getString("date"));
 				post.put("position", mResultSet.getString("position"));
 				post.put("post_image", mResultSet.getString("image"));
+				post.put("comment", mResultSet.getString("text"));
 				post.put("post_video", "null");
 				post.put("likes", "null");		
-				posts.put(post);
+
+				postsList.put(post);
+
 			}
-			closeConnection();
+
+			for(int i = 0; i < postsList.length(); i++)
+			{
+				JSONObject ppp = (JSONObject) postsList.get(i);
+				final String q = "select nickname,userimage from user where email = ?;";
+
+				final PreparedStatement prepState = mConnection.prepareStatement(q);
+				prepState.setString(1, ppp.getString("user_nick"));
+
+				final ResultSet resSet = prepState.executeQuery();
+				while (resSet.next()) {
+
+					ppp.put("user_nick", resSet.getString("nickname"));
+					ppp.put("user_image", resSet.getString("userimage"));
+
+				}
+			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		return posts;
+
+
+		closeConnection();
+
+
+		return postsList;
 	}
 
 	public boolean createPost(final String type, final String position, final String image, final String userEmail, final String text) {
-		
+
 		//final String query = "INSERT INTO `yellit`.`post` (`data`, `title`, `subtitle`, `type`, `text`, `location`) VALUES (?, ?, ?, ?, ?, ?, ?);";
-		
+
 		final String query = "INSERT INTO post (post.type, post.position, post.image, post.user_email, post.text) VALUES (?, ?, ?, ?, ?);";
 		try {
-			
+
 			final Connection mConnection = createConnection();
 			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
 			mPreparedStatement.setString(1, type);
@@ -66,16 +88,16 @@ public class PostConnection extends AbstractDBManager {
 			mPreparedStatement.setString(4, userEmail);
 			mPreparedStatement.setString(5, text);
 			mPreparedStatement.execute();
-			
+
 			closeConnection();
-			
+
 			return true;
-			
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 			return false;
-			
+
 		} 		
 	}
 
@@ -95,7 +117,7 @@ public class PostConnection extends AbstractDBManager {
 		}
 		return deletePost;
 	}
-	
+
 	public JSONArray getPersonalPost(final String email) {
 		final JSONArray posts = new JSONArray();
 		final String query = "select count(*), type from post where user_email = ? group by type ;";
@@ -109,7 +131,7 @@ public class PostConnection extends AbstractDBManager {
 				final JSONObject post = new JSONObject();
 				post.put("", mResultSet.getString("count(*)"));
 				post.put("", mResultSet.getString("type"));	
-				
+
 				posts.put(post);
 			}
 			closeConnection();
