dfcb1c6a01e390148505d8939747549f85396b56
lorenzobrusco <lollobrusco@hotmail.it>
2017-07-14 15:06:11 +0200

    fixed error sign in and like

--- src/connection/LikeConnection.java
+++ src/connection/LikeConnection.java
@@ -11,15 +11,50 @@ import org.json.JSONObject;
 
 import com.sun.xml.internal.fastinfoset.util.StringArray;
 
+import tools.BaseUrl;
+
 public class LikeConnection extends AbstractDBManager {
 
 	public LikeConnection() {
 		super();
 	}
-	
+
+	public JSONObject isLike(final int idPost, final String email) {
+		JSONObject updatedLikes = new JSONObject();
+		final String query = "SELECT count(*) as total FROM " + BaseUrl.DB + "like WHERE user_email=? and post_idpost = ?; ";
+		int result = 0;
+		try {
+			final Connection mConnection = createConnection();
+			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
+			mPreparedStatement.setString(1, email);
+			mPreparedStatement.setInt(2, idPost);
+			mPreparedStatement.execute();
+			final ResultSet mResultSet = mPreparedStatement.executeQuery();
+			while (mResultSet.next()) {
+				result = mResultSet.getInt("total");
+			}
+			updatedLikes.put("isLike", result);
+			closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return updatedLikes;
+	}
+
+	/**
+	 * Add one like
+	 * 
+	 * @param idPost
+	 * @param email
+	 * @param emailPost
+	 * @return
+	 */
 	public JSONObject addLike(final int idPost, final String email, final String emailPost) {
 		JSONObject updatedLikes = new JSONObject();
-		final String query = "INSERT INTO yellit.like (`user_email`, `post_idpost` , `post_user_email`) VALUES (?, ?, ?);";
+		final String query = "INSERT INTO " + BaseUrl.DB + "like (`user_email`, `post_idpost` , `post_user_email`) VALUES (?, ?, ?);";
 		try {
 			final Connection mConnection = createConnection();
 			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
@@ -30,43 +65,54 @@ public class LikeConnection extends AbstractDBManager {
 			int updated = getLikesPost(idPost, mConnection);
 			updatedLikes.put("likes", updated);
 			closeConnection();
-			}
-		catch (SQLException e) {
-		e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
+
 		return updatedLikes;
 	}
-	
+
+	/**
+	 * Remove like
+	 * 
+	 * @param idPost
+	 * @param email
+	 * @param emailPost
+	 * @return
+	 */
 	public JSONObject removeLike(final int idPost, final String email, final String emailPost) {
 		JSONObject updatedLikes = new JSONObject();
-		final String query = "DELETE FROM yellit.like WHERE user_email=? and post_idpost = ? and post_user_email = ?;";
+		final String query = "DELETE FROM " + BaseUrl.DB + "like WHERE user_email=? and post_idpost = ?;";
 		try {
 			final Connection mConnection = createConnection();
 			final PreparedStatement mPreparedStatement = mConnection.prepareStatement(query);
 			mPreparedStatement.setString(1, email);
 			mPreparedStatement.setInt(2, idPost);
-			mPreparedStatement.setString(3, emailPost);
 			mPreparedStatement.execute();
 			int updated = getLikesPost(idPost, mConnection);
 			updatedLikes.put("likes", updated);
 			closeConnection();
-			}
-		catch (SQLException e) {
-		e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
+
 		return updatedLikes;
 	}
-	
-	
+
+	/**
+	 * Return all likes of one post
+	 * 
+	 * @param idPost
+	 * @param mConnection
+	 * @return
+	 */
 	public int getLikesPost(final int idPost, Connection mConnection) {
-		
-		final String query = "select count(*) as total from yellit.like where post_idpost = ?; ";
+
+		final String query = "select count(*) as total from " + BaseUrl.DB + "like where post_idpost = ?; ";
 		int result = 0;
 		try {
 
@@ -74,16 +120,14 @@ public class LikeConnection extends AbstractDBManager {
 			mPreparedStatement.setInt(1, idPost);
 			final ResultSet mResultSet = mPreparedStatement.executeQuery();
 			while (mResultSet.next()) {
-				
+
 				result = mResultSet.getInt("total");
 			}
 
 		} catch (SQLException e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		}
 		return result;
 	}
 
-	
-	
 }
